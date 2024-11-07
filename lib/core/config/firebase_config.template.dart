8068a8aba86afb9f5a9a79f99ee05747ca2acd18
aidanoss <aidanharitos@live.com>
2024-11-07 21:03:47 +0800

    Update .gitignore and add Firebase configuration template

--- lib/core/config/firebase_config.template.dart
+++ lib/core/config/firebase_config.template.dart
@@ -5,12 +5,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirebaseConfig {
   static const FirebaseOptions firebaseOptions = FirebaseOptions(
-    // TODO: Replace these values with your Firebase project configuration
     apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
+    authDomain: 'YOUR_AUTH_DOMAIN',
     projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-    messagingSenderId: 'YOUR_SENDER_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
     appId: 'YOUR_APP_ID',
     measurementId: 'YOUR_MEASUREMENT_ID',
   );
